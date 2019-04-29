faa945f4513907dac2b0c4ca4aa3d5eb1f5d9b3d
Ruarai Kirk <ruaraikirk@gmail.com>
2019-04-29 11:00:05 +0100

    Adding Netlify form handling.

--- src/components/Main.js
+++ src/components/Main.js
@@ -52,7 +52,8 @@ class Main extends React.Component {
 
         <article id="contact" className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Contact</h2>
-          <form method="post" action="#">
+          {/*<form method="post" action="#">*/}
+          <form name="contact" method="POST" data-netlify="true">
             <div className="field half first">
               <label htmlFor="name">Name</label>
               <input type="text" name="name" id="name" />
