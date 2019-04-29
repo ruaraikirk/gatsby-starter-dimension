a05c1e68a8c48b93055031f4ecad4af86f99e369
Ruarai Kirk <ruaraikirk@gmail.com>
2019-04-29 12:51:24 +0100

    Initial commit for form handling.

--- src/components/Main.js
+++ src/components/Main.js
@@ -53,7 +53,7 @@ class Main extends React.Component {
 
         <article id="contact" className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Contact</h2> 
-          <form method="post" action="#">
+          <form name="contact" method="POST" action="/page-2/" data-netlify="true">
             <div className="field half first">
               <label htmlFor="name">Name</label>
               <input type="text" name="name" id="name" />
